493e965d9abea0a4396f653f66b6e9202c7d04e3
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-12-13 19:23:09 +0000

    Gramatical changes in the storyline

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def clear_terminal():
 
 def game_over():
     """
-    When the player don't want to continue playing the game
+    When the player doesn't want to continue playing the game
     """
     print("Hopefully we will get the chance to meet again..")
     sys.exit()
@@ -57,7 +57,7 @@ def play_game():
     print("find the truth? (1 or 2):")
     print("--------------------")
     print("- 1 - Hell yeah!")
-    print("- 2 -  No to much preasure..")
+    print("- 2 -  No, too much pressure..")
     print("---------------------")
     print()
 
@@ -89,7 +89,7 @@ def introduction():
     """
     clear_terminal()
     print(f"Growing up the famous treasure von {lname} was all you could")
-    print("here about. But when you asked where it was you never")
+    print("hear about. But when you asked where it was you never")
     print("got an answer. All they said was --you will find out")
     print("in good ol' time-- ")
     print()
@@ -100,11 +100,11 @@ def introduction():
     print("You thought the information inside it was useless but")
     print("for some reason you couldn't get that sigil out")
     print("of your mind. It looked like a heart pierced by two daggers! ")
-    print("When you turned the envelope you could see an adress on the back.")
-    print("This adress might be the answer to the family legend..")
+    print("When you turned the envelope you could see an address on the back.")
+    print("This address might be the answer to the family legend..")
     print()
     print("---------------------------------------")
-    print("-- 1 --  No time to loose lets go!")
+    print("-- 1 --  No time to lose, let's go!")
     print("-- 2 -- This adventure is not meant for me..")
     print("---------------------------------------")
     print()
@@ -127,11 +127,11 @@ def start():
     Gives the player their first choice which will affect their journey
     """
     print(f"\n After a couple of hours of traveling {fname} was standing")
-    print("infront of a huge badly maintained mansion. So this was the")
+    print("in front of a huge badly maintained mansion. So this was the")
     print(f" famous von {lname} manor. {fname} moved up the stairs and ")
     print("pushed down the door handle. It worked!")
     print(f"{fname} walked inside and was overwhelmed by the interior.")
-    print("It was such a beautiful place, why did our family ever leave!?")
+    print("It was such a beautiful place, why did our family ever leave?")
     print()
     print("There were two different doors that stood out, one leading to the")
     print("greenhouse and the other to the library.)
@@ -149,7 +149,7 @@ def greenhouse():
     print("You navigate your way inside the greenhouse,")
     print("avoiding all the debris laying on the ground.")
     print("[fname] starts to move the flashlight up and down")
-    print("and notice a spiral staircase hidden in the corner.")
+    print("and notices a spiral staircase hidden in the corner.")
     print("It is old and rusty and have a specific pattern ")
     print("engraved in the steel that reminds you of stars and moons")
     print("Suddenly you hear a sound behind you and when you start ")
@@ -167,8 +167,8 @@ def greenhouse():
 def fountain():
     """
     """
-    print("You take a couple of steps forward with the heart pumping")
-    print("in your chest. The sound came from underneath a large box")
+    print("You take a couple of steps forward with your heart pumping")
+    print("through your chest. The sound came from underneath a large box")
     print("placed next to the fountain, after taking a deep breath")
     print(" you decide to kick the box and a rat trapped underneath")
     print("makes you jump. When you calm down you see some types of symbols")
@@ -192,11 +192,11 @@ def library():
     """
     print("\nAfter taking your first steps inside the library you feel")
     print("a cold chill moving down your spine and you know instantly")
-    print("that you are on the right place. You try to flip a switch")
+    print("that you are in the right place. You try to flip a switch")
     print("and to your relief a dim light turns on and you move towards")
-    print("the bookcase. When you starting to scan the titles you see an")
-    print("anstronomy book an your curiosity takes over. When you start")
-    print("pulling the book out the picutre to the right changes and a")
+    print("the bookcase. When you start to scan the titles you see an")
+    print("astronomy book and your curiosity takes over. When you start")
+    print("pulling the book out the picture to the right changes and an")
     print("old rusty lever emerges. You are now so close but you hesitate,")
     print("is continuing really the right option? ")
     print("---------------------------------------")
@@ -210,18 +210,18 @@ def hiddenroom():
     """
     """
     print("You hear a click when pulling down the lever and the wall")
-    print("inside the fireplace disappear. You decide that it's now or")
+    print("inside the fireplace disappears. You decide that it's now or")
     print("never and head inside! ")
     print("After removing some cobwebs you turn on the flashlight and ")
     print("start looking around, you stop when you see a picture of ")
     print("your great-grandparents hanging above the chest.")
-    print("The light of your flashlight reveals a inscription ")
-    print("below the picture, so the family legend was true!?")
+    print("The light of your flashlight reveals an inscription ")
+    print("below the picture, so the family legend was true?!")
     print()
-    print(" -- When the planets are in alignment an important choice will ")
+    print(" -- When the planets are aligned an important choice will ")
     print("be clear. Always remember, blood is thicker than water. --")
     print()
-    print("You brush of the dust on the chest and two keyholes is revealed ")
+    print("You brush off the dust on the chest and two keyholes are revealed ")
     print("with two images above. The one to the left displays a heart ")
     print("that are being pierced by two daggers, the one to the right")
     print("shows a blowing leaf with a crown attached to the stem.")
@@ -237,7 +237,7 @@ def treasure():
     """
     print("You decide to go with the blowing leaf and to your ")
     print("excitement you open the chest and finds it containing")
-    print("more gold then you ever seen. As you start to fill ")
+    print("more gold than you have ever seen. As you start to fill ")
     print("your pockets with the gold you see a red light beaming")
     print("from the lid. You have no time to think before you get")
     print("sucked inside the chest and all is black")
@@ -247,12 +247,12 @@ def treasure():
 def spirits():
     """
     """
-    print("You decide to go with the heart piercied with two daggers ")
+    print("You decide to go with the heart pierced with two daggers ")
     print("and as you turn around the key you find the chest eampty. ")
     print("You try to hide your disapointment and are about to leave ")
     print("when you see a blue light beam from the lid. You have ")
-    print("manages to free the souls of your grandparents, a warm ")
-    print("feeling replaces the cold you had before. Finally the curse")
+    print("managed to free the souls of your grandparents, a warm ")
+    print("feeling replaces the coldness you felt before. Finally the curse")
     print("is over!")
     game_over
 
