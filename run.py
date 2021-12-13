09f4d0db317d9c0b334ae3b22befd4a226ce5055
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-12-13 09:10:14 +0000

    Introducing the introduction which serves as an background story for the player

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def introduction (): 
+    #Introducing a storyline for the player where they have the, 
+    #opportunity to chose to embark on the adventure
+    print("\nGrowing up you never knew much about your family history")
+    print("and that was why you were surprised to find a suspicious")
+    print("envelope in the mail a couple of weeks ago.")
+    print("The information inside it was confusing you even more")
+    print("but the thing that you couldn't shake from your mind")
+    print("was the the picture drawn in the bottom of the page.")
+    print("It was a family chest with a heart pierced by two daggers. ")
+
+    
+
+
 def start ():
     # Introducing the storyline for the player
-    print("\n ")
+    print("\n The legend about this house has been passed down for generations.")
+    print("The legend about the family chest that is hidden somewhere inside the  has long been a subject for")
+    print("discussion in the [lname] household.")
+    print("")
+
 print("\n ")
