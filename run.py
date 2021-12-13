dfd1d24b91bd65d01220b7e64a291f328a239ab6
MadeleneEriksson <madelene__eriksson@hotmail.com>
2021-12-13 09:22:04 +0000

    Written down the story for the greenhouse room

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-def introduction (): 
+def introduction(): 
     #Introducing a storyline for the player where they have the, 
     #opportunity to chose to embark on the adventure
     print("\nGrowing up you never knew much about your family history")
@@ -12,14 +12,28 @@ def introduction ():
     print("was the the picture drawn in the bottom of the page.")
     print("It was a family chest with a heart pierced by two daggers. ")
 
-    
-
-
-def start ():
-    # Introducing the storyline for the player
+def start():
+    # Gives the player their first choice which will affect their journey 
     print("\n The legend about this house has been passed down for generations.")
     print("The legend about the family chest that is hidden somewhere inside the  has long been a subject for")
     print("discussion in the [lname] household.")
     print("")
 
-print("\n ")
+def greenhouse():
+    # 
+    print("You navigate your way inside the greenhouse,")
+    print("avoiding all the debris laying on the ground.")
+    print("[fname] starts to move the flashlight up and down")
+    print("and notice a spiral staircase hidden in the corner.")
+    print("It is old and rusty and have a specific pattern ")
+    print("engraved in the steel that reminds you of stars and moons")
+    print("Suddenly you hear a sound behind you and when you start ")
+    print("to turn around you see something move next to the fountain.")
+    print("You can either continue and make your way to the staircase or")
+    print(" investigate what moved next to the fountain.")
+    print("What will be your choice? (1 or 2)")
+    print("1.) Staircase")
+    print("2.) Fountain")
+    
+
+print("\n Hi")
